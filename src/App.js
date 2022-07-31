53599b677e2beab79752700b7ac9c88b29ca92fa
Brave0211 <abduqodirovjasurbek11@gmail.com>
2022-07-31 11:32:43 +0500

    git commit -m "react-todo"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useRef, useState } from "react";
+import { List } from "./components";
+import { Item } from "./components";
 
 function App() {
+
+  const elInput = useRef(null)
+
+  const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem("todos")) || []);
+
+  let all = todos.length
+	let com = todos.filter(el => el.isComplated).length
+	let uncom = todos.filter(el => !el.isComplated).length
+
+  const handleInputValue = (evt) => {
+    evt.preventDefault()
+    const newTodo = {
+      id: todos.at(-1)?.id ? todos.at(-1).id + 1 : 1,
+      text: elInput.current.value,
+      isComplated: false
+    };
+    setTodos([...todos, newTodo]);
+    elInput.current.value = null;
+  };
+
+
+
+  window.localStorage.setItem("todos", JSON.stringify(todos))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleInputValue}>
+        <input ref={elInput} type="text" placeholder="Todo..." />
+        <button type="submit">Submit</button>
+        </form>
+        {todos.length > 0 && <List>
+          {todos.map((e) => (
+            <Item key={e.id} item={e} todos={todos} setTodos={setTodos} />
+          ))}
+        </List>}
+        <button type="button">All <strong className="all">{all}</strong></button>
+        <button type="button">Complate <strong className="com">{com}</strong></button>
+        <button type="button">Uncomplate <strong className="uncom">{uncom}</strong></button>
+      </div>
     </div>
   );
 }
